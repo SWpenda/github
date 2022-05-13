ea617636a4dbcad000deaa71a55cec5fdb63bb6a
k2k1997 <j59160859@gmail.com>
2022-05-13 18:51:03 +0900

    This is second commit test!

---
+++ src/githubtest/This_is_only_github2.java
@@ -0,0 +1,11 @@
+package githubtest;
+interface A{
+	This_is_only_github2ublic class This_is_only_github {
+	public static void main(String[]ar) {
+		A a = new A() {
+			a.abc(){
+				System.out.println("git");
+			}
+		};
+	}
+}
