752e97c708fd027cbee206495376c81d2cf8d23a
k2k1997 <j59160859@gmail.com>
2022-05-16 10:43:49 +0900

    ㅁㄴㅇㅁ

--- src/githubtest/This_is_only_github.java
+++ src/githubtest/This_is_only_github.java
@@ -1,5 +1,19 @@
 package githubtest;
 
-public class This_is_only_github {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+public class This_is_only_github {
+	public static void main(String[]args) {
+		List<String> product = new ArrayList<>();
+		product.add("virtual");
+		product.add("real");
+		product.add("financal");
+		
+		for(int i=0;i<product.size();i++) {
+			String value = product.get(i);
+			System.out.println(value);//에러가 뜬다.
+		}
+	}
 }
